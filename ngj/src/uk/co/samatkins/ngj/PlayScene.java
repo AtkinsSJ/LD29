3ef00036a4e5cad195ea1b92f8462b4acaf10052
Samuel Atkins <github@samatkins.co.uk>
2014-04-27 14:46:46 +0100

    Started making things look pretty. Water renders, and there's a flag to disable box2d's debug renderer.

--- ngj/src/uk/co/samatkins/ngj/PlayScene.java
+++ ngj/src/uk/co/samatkins/ngj/PlayScene.java
@@ -1,9 +1,13 @@
 package uk.co.samatkins.ngj;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Array;
 import uk.co.samatkins.Entity;
 import uk.co.samatkins.Scene;
 import uk.co.samatkins.components.graphics.SpriteComponent;
@@ -14,16 +18,23 @@ import uk.co.samatkins.components.graphics.SpriteComponent;
 public class PlayScene extends Scene<NGJGame> {
     public static final String endFixtureID = "End",
                                 playerFixtureID = "Player";
+    public static final Color SKY_COLOR = Color.valueOf("5BCEFF"),
+                            WATER_COLOR = Color.valueOf("3A5EFF");
 
     private World world;
+    private Array<Body> waterBodies;
+    private float particleRadius;
     private Box2DDebugRenderer debugRenderer;
     public static final float TIME_STEP = 1f / 30f;
     private float physicsCounter = 0;
     private FPSLogger fpsLogger;
     private Entity swanEntity;
 
+    private static final boolean DRAW_DEBUG = true;
+
     public PlayScene(NGJGame game) {
         super(game);
+        Gdx.gl.glClearColor(SKY_COLOR.r, SKY_COLOR.g, SKY_COLOR.b, 1f);
         this.fpsLogger = new FPSLogger();
 
         this.world = new World(new Vector2(0, -10), true);
@@ -74,6 +85,8 @@ public class PlayScene extends Scene<NGJGame> {
      */
     private void createWater(float x, float y, float width, float height, float particleRadius) {
 
+        this.waterBodies = new Array<Body>();
+
         CircleShape shape = new CircleShape();
         shape.setRadius(particleRadius);
         FixtureDef fixtureDef = new FixtureDef();
@@ -82,6 +95,7 @@ public class PlayScene extends Scene<NGJGame> {
         fixtureDef.friction = 0.1f;
         fixtureDef.restitution = 0.6f;
 
+        this.particleRadius = particleRadius;
         float diameter = particleRadius * 2;
         int across = (int) (width/diameter),
             up = (int) Math.ceil(height/diameter);
@@ -100,6 +114,7 @@ public class PlayScene extends Scene<NGJGame> {
 
                 Body body = world.createBody(bodyDef);
                 body.createFixture(fixtureDef);
+                waterBodies.add(body);
             }
         }
 
@@ -141,11 +156,27 @@ public class PlayScene extends Scene<NGJGame> {
     @Override
     public void draw() {
         getCamera().position.set(swanEntity.getX(), swanEntity.getY(), 0);
-        super.draw();
-        this.debugRenderer.render(world, getCamera().combined.scl(2));
-        getCamera().combined.scl(0.5f);
-        fpsLogger.log();
 
+        // Draw background and water
+        ShapeRenderer shapeRenderer = getShapeRenderer();
+        shapeRenderer.setProjectionMatrix(getCamera().combined.scl(2));
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(WATER_COLOR);
+
+        for (Body body: waterBodies) {
+            shapeRenderer.circle(body.getPosition().x, body.getPosition().y, particleRadius);
+        }
+
+        shapeRenderer.end();
+        getCamera().combined.scl(0.5f);
+
+        super.draw();
+
+        if (DRAW_DEBUG) {
+            this.debugRenderer.render(world, getCamera().combined.scl(2));
+            getCamera().combined.scl(0.5f);
+            fpsLogger.log();
+        }
     }
 
     public void gameWon() {
